bdedd9d2e1408c033954f9b48007d4e9bc50c862
bigblackbean <bigblackbean@naver.com>
2024-01-18 23:49:58 +0900

    [세팅 중] 배경지정, 네비게이션 작업 중

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,15 +1,29 @@
 import Link from "next/link";
+import styles from "../styles/navigation.module.css";
+import Image from "next/image";
+
+function NavigationItem(props: { name: string; path: string }) {
+  return (
+    <li>
+      <Link href={props.path}>
+        <Image src={""} alt="" />
+        <p style={{ textAlign: "center", color: "#fff" }}>{props.name}</p>
+      </Link>
+    </li>
+  );
+}
 
 export default function Navigation() {
+  const NAVIGATION_LIST: { id: number; name: string; path: string }[] = [
+    { id: 0, name: "Profile", path: "profile" },
+    { id: 1, name: "Skills", path: "skills" },
+  ];
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li>
-          <Link href="/skills">Skills</Link>
-        </li>
+      <ul style={{ display: "flex", justifyContent: "center" }}>
+        {NAVIGATION_LIST.map((item) => (
+          <NavigationItem key={item.id} name={item.name} path={item.path} />
+        ))}
       </ul>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Navigation from "./components/Navigation";
+import styles from "./page.module.css";
 
 export default function Home({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
-      <Navigation />
+      <div className={styles.main}>
+        <h2 className={styles.title}>PORTFOLIO</h2>
+        {children}
+        <div style={{ width: "100%", position: "fixed", bottom: 0 }}>
+          <Navigation />
+        </div>
+      </div>
     </>
   );
 }
