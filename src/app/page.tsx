990dbaca64dfffc51990a84aa932808d294fddbc
bigblackbean <bigblackbean@naver.com>
2024-12-20 14:48:34 +0900

    [레이아웃/네비] 기초레이아웃 작업 완료, 네비 아이콘 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 export default function Home() {
   return (
     <div>
-      <p
+      <div
         style={{
-          fontSize: 40,
-          fontWeight: "bold",
-          color: "#fff",
-          position: "relative",
-          zIndex: 1,
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
         }}
       >
-        PORTFOLIO
-      </p>
+        <p
+          style={{
+            fontSize: 40,
+            fontWeight: "bold",
+            color: "#fff",
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          PORTFOLIO
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,19 @@
-import Link from "next/link";
 import { FunctionComponent } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+import user from "@/images/icons/user.svg";
+import paper from "@/images/icons/paper.svg";
+import folder from "@/images/icons/folder.svg";
+import ppt from "@/images/icons/ppt.svg";
+import film from "@/images/icons/film.svg";
 
 const navList = [
-  { name: "profile", url: "/profile" },
-  { name: "career", url: "/career" },
-  { name: "contents", url: "/contents" },
-  { name: "service", url: "/service" },
-  { name: "history", url: "/history" },
+  { name: "Profile", url: "/profile", image: user },
+  { name: "Career", url: "/career", image: paper },
+  { name: "Contents", url: "/contents", image: folder },
+  { name: "Lesson", url: "/lesson", image: ppt },
+  { name: "History", url: "/history", image: film },
 ];
 
 const Nav: FunctionComponent = () => {
@@ -21,19 +28,33 @@ const Nav: FunctionComponent = () => {
         zIndex: 10,
       }}
     >
-      <ul style={{ display: "flex" }}>
+      <ul style={{ display: "flex", padding: 8 }}>
         {navList.map((item) => (
           <li key={item.name} style={{ width: 64, height: 64 }}>
             <Link href={item.url}>
               <div
                 style={{
-                  display: "block",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gap: 4,
                   width: "100%",
                   height: "100%",
-                  border: "1px solid #ccc",
                 }}
               >
-                <p style={{ textAlign: "center", color: "#fff" }}>
+                <div style={{ width: 32, height: 32 }}>
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={item.image}
+                    alt={item.name}
+                  />
+                </div>
+                <p style={{ fontSize: 12, textAlign: "center", color: "#fff" }}>
                   {item.name}
                 </p>
               </div>
