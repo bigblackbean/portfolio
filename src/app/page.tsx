c30d9d914ff3342ae1934f934e6a812db36139be
bigblackbean <bigblackbean@naver.com>
2025-06-09 00:16:02 +0900

    불필요한 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,25 +6,6 @@ import { useEffect, useState } from "react";
 import Main from "@/components/main";
 
 export default function Home() {
-  const [clicked, setClicked] = useState<boolean>(false);
-
-  const next = () => {
-    setClicked(true);
-    sessionStorage.setItem("visited", "true");
-  };
-
-  const back = () => {
-    setClicked(false);
-    sessionStorage.setItem("visited", "false");
-  };
-
-  useEffect(() => {
-    const visited = sessionStorage.getItem("visited");
-    if (visited === "true") {
-      setClicked(true);
-    }
-  }, []);
-
   return (
     <div>
       <Main />
